283bc441f11eae4414b09d781bf22527bdaf911b
cutiefunny <cutiefunny@gmail.com>
2025-12-01 13:33:05 +0900

    manifest 경로 변경하여 재업

--- vite.config.js
+++ vite.config.js
@@ -6,29 +6,40 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
-			registerType: 'autoUpdate', // 업데이트 시 자동 새로고침
-			includeAssets: ['favicon.svg', 'robots.txt'], // 캐싱할 정적 파일
+			srcDir: './src',
+			mode: 'development',
+			strategies: 'generateSW',
+			scope: '/',
+			base: '/',
+			selfDestroying: process.env.SW_DESTROY === 'true',
 			manifest: {
-				name: 'MuscleCat Drawing',
-				short_name: '근육고양이 그림판',
-				description: '간단하게 그림을 그리는 앱이다!',
+				short_name: '근육고양이 낙서장',
+				name: '근육고양이 낙서장',
+				start_url: '/',
+				display: 'standalone',
 				theme_color: '#ffffff',
 				background_color: '#ffffff',
-				display: 'standalone', // 브라우저 UI 없이 앱처럼 실행
-				scope: '/',
-				start_url: '/',
 				icons: [
 					{
-						src: 'pwa-192x192.png',
+						src: '/pwa-192x192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: 'pwa-512x512.png',
+						src: '/pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					}
 				]
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			devOptions: {
+				enabled: true,
+				suppressWarnings: true,
+				navigateFallback: '/',
+				type: 'module'
 			}
 		})
 	]
